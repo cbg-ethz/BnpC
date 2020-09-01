Underflow bugfix in log prob normalization

libs/plotting.py
@@ -52,30 +52,19 @@ def get_colors(n, cmap='gist_rainbow', scale=0.85, alternating=True):
 
 
 def _get_col_order(assignment):
-    clusters = []
-    cluster_idx = []
-    doublets = np.zeros(len(assignment), dtype=int)
-
-    for i, cl in enumerate(assignment):
-        if isinstance(cl, (list, tuple, np.ndarray)):
-            doublets[i] = 1
-        elif cl not in clusters:
-            clusters.append(cl)
-            cluster_idx.append(i)
-
+    clusters, cluster_cnt = np.unique(assignment, return_counts=True)
+   
     col_order = np.array([], dtype=int)
-    for cl_idx in np.argsort(cluster_idx):
+    for cl_idx in np.argsort(cluster_cnt)[::-1]:
         cols = [i for i, j in enumerate(assignment) \
-            if isinstance(j, (int, np.integer)) and j == clusters[cl_idx]]
+            if j == clusters[cl_idx]]
         col_order = np.append(col_order, cols)
 
-    # Append doublet cells as last columns
-    col_order = np.append(col_order, np.where(doublets == 1))
     return col_order
 
 
 def plot_raw_data(data_in, data_raw_in=pd.DataFrame(), out_file=None,
-            assignment=np.array([]), metric='correlation', row_cl=False):
+            assignment=np.array([]), metric='correlation', row_cl=True):
 
     data = data_in.copy()
     data_raw = data_raw_in.copy()
@@ -87,17 +76,20 @@ def plot_raw_data(data_in, data_raw_in=pd.DataFrame(), out_file=None,
     if len(assignment) > 0:
         col_order = _get_col_order(assignment)
 
-        clusters, cl_idx = np.unique(assignment, return_index=True)
+        clusters, cl_cnt = np.unique(assignment, return_counts=True)
+
         if clusters.size > len(COLORS):
-            colors = get_colors(clusters.size)
-            col_map = {i: next(colors) for i in clusters[np.argsort(cl_idx)]}
-        else:
-            col_map = {
-                j: COLORS[i] for i,j in enumerate(clusters[np.argsort(cl_idx)])
-            }
+            colors = get_colors(clusters.size - len(COLORS))
+
+        col_map =  {}
+        for i, j in enumerate(clusters[np.argsort(cl_cnt)[::-1]]):
+            try:
+                col_map[j] = COLORS[i] 
+            except IndexError:
+                col_map[j] = next(colors)
 
         col_dict = np.full(data_in.shape[1], '#ffffff', dtype='<U7')
-        for i, cl in enumerate(data_in.columns[col_order]):
+        for i, cl in enumerate(col_order):
             col_dict[i] = col_map[assignment[cl]]
                 
         cluster_cols = pd.Series(col_dict, name='clusters', index=col_order)
@@ -116,12 +108,8 @@ def plot_raw_data(data_in, data_raw_in=pd.DataFrame(), out_file=None,
         x_labels = data_in.columns
 
     if row_cl:
-        if not data_raw.empty:
-            Z = linkage(data_raw.fillna(3), 'ward')
-            row_order = dendrogram(Z, truncate_mode=None)['leaves']
-        else:
-            Z = linkage(data.fillna(3), 'ward')
-            row_order = dendrogram(Z, truncate_mode=None)['leaves']
+        Z = linkage(data.fillna(3), 'complete')
+        row_order = dendrogram(Z, truncate_mode=None)['leaves']
 
         data = data.iloc[row_order]
         if not data_raw.empty:
@@ -143,8 +131,10 @@ def plot_raw_data(data_in, data_raw_in=pd.DataFrame(), out_file=None,
     else:
         annot = False
 
-    cmap = plt.get_cmap('bwr', 100)
+    # cmap = plt.get_cmap('bwr', 100)
     # cmap = plt.get_cmap('Reds', 100)
+    cmap = plt.get_cmap('Reds', 2)
+
     cmap.set_over('green')
     cmap.set_bad('grey')
 
@@ -163,6 +153,10 @@ def plot_raw_data(data_in, data_raw_in=pd.DataFrame(), out_file=None,
 
     cm.ax_heatmap.set_yticks(np.arange(0.5, data.shape[0], 1))
     cm.ax_heatmap.set_xticks(np.arange(0.5, data.shape[1], 1))
+
+    # TODO <NB> remove in production
+    # cm.ax_heatmap.set_yticks([])
+    # cm.ax_heatmap.set_xticks([])
 
     cm.ax_heatmap.set_xticklabels(x_labels, rotation=90, fontsize=8)
     cm.ax_heatmap.set_yticklabels(data_in.index, fontsize=8)
@@ -381,5 +375,44 @@ def stdout_fig(fig, out_file, dpi=300):
         plt.close()
 
 
+def load_txt(path):
+    try:
+        df = pd.read_csv(path, sep='\t', index_col=False)
+        x = df.at[0, 'Assignment'].split(' ')
+    except ValueError:
+        with open(path, 'r') as f:
+            x = f.read().split(' ')
+
+    l = []
+    while x:
+        l.append(int(x.pop()))
+    return l[::-1]
+
+
 if __name__ == '__main__':
+    data_dir = '/home/hw/Desktop/crp_clustering/data/McPherson2016/'
+    # data_dir = '/home/hw/Desktop/crp_clustering/data/Gawad2014/Patient4/'
+    data_dir = '/home/hw/Desktop/crp_clustering/data/Wu2016/CRC0827/'
+    data_dir = '/home/hw/Desktop/crp_clustering/data/Wu2016/CRC0907/'
+
+    run_dir = os.path.join(data_dir, '20200522_17:23:18')
+    geno_file = os.path.join(run_dir, 'genotypes_posterior_mean.tsv')
+    geno = pd.read_csv(geno_file, sep='\t', index_col=0, header=0)
+    geno.columns = np.arange(geno.columns.size)
+
+    assign_file = os.path.join(run_dir, 'assignment.txt')
+    assign = load_txt(assign_file)
+
+    # data_raw_file = os.path.join(data_dir, 'patient.csv')
+    # data_raw = pd.read_csv(data_raw_file, sep=' ', index_col=None, header=None)
+    data_raw = pd.DataFrame()
+
+    out_file = 'Wu907_test.png'
+
+    plot_raw_data(geno, data_raw, assignment=assign, out_file=out_file, row_cl=False)
+    
+
+
+
     print('Here be dragons...')
+

libs/utils.py
@@ -102,7 +102,8 @@ def _get_MPEAR(assignments):
     dist = squareform(dist)
 
     avg_cl_no = np.mean([np.unique(i).size for i in assignments])
-    n_range = np.arange(avg_cl_no * 0.25, avg_cl_no * 1.25, dtype=int)
+    n_range = np.arange(max(2, avg_cl_no * 0.2),
+        min(avg_cl_no * 2.5, assignments.shape[1]), dtype=int)
 
     best_MPEAR = -np.inf
     best_assignment = None
@@ -115,6 +116,7 @@ def _get_MPEAR(assignments):
         if score > best_MPEAR:
             best_assignment = model.labels_
             best_MPEAR = score
+            print(f'\tbest n: {n}')
     return best_assignment
 
 
@@ -420,7 +422,7 @@ def get_lugsail_batch_means_est(data_in, steps=None):
 
     for data_chain, burnin_chain in data_in:
         data = data_chain[burnin_chain:steps]
-        if data.size < 2:
+        if data.size < 9: # otherwise b // 3 == 0
             return np.inf
         # [chapter 2.2 in Vats and Knudson, 2018]
         n_ii = data.size
@@ -450,7 +452,6 @@ def get_tau_lugsail(b, data, chain_mean):
     a = data.size // b # Number of batches
     batch_mean = bn.nanmean(np.reshape(data[:a * b], (a, b)), axis=1)
     return (b / (a - 1)) * bn.nansum(np.square(batch_mean - chain_mean))
-
 
 
 def get_cutoff_lugsail(e, a=0.05):